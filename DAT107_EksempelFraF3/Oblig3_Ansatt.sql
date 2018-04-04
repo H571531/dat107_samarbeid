1764a46487fbd9fd0b28c97aee47ec9ab89d7bb7
H571531 <31210153+H571531@users.noreply.github.com>
2018-04-04 11:41:15 +0200

    Push SQL og Ansatt

---
+++ DAT107_EksempelFraF3/Oblig3_Ansatt.sql
@@ -0,0 +1,26 @@
+DROP SCHEMA IF EXISTS Oblig3 CASCADE;
+
+CREATE SCHEMA Oblig3;
+SET search_path TO Oblig3;
+    
+-- Ikke nødvendig å sletter tabellen(e) siden vi har tomt skjema, men ...
+-- DROP TABLE person;
+
+CREATE TABLE Ansatt
+(
+    AnsattId SERIAL,
+	brukernavn VARCHAR(6),
+	fornavn VARCHAR(30),
+	etternavn VARCHAR(30),
+    AnsettelsesDato DATE,
+	Stilling VARCHAR(30) NOT NULL,
+	manedslonn DECIMAL(10, 2),
+    CONSTRAINT Ansatt_PK PRIMARY KEY (AnsattId)
+);
+
+INSERT INTO
+  Ansatt(brukernavn,fornavn,etternavn,AnsettelsesDato,Stilling,manedslonn)
+VALUES
+    ('adrmor','Adrian','Mortensen','2008-11-11','Tekniker','32000.2'),
+    ('adrjor','Adrian','Johnsen','2008-11-11','Selger','38000.2'),
+    ('adrren','Adrian','Rene','2008-11-11','Sjef','38000.2');
--- Dat107_Oblig3/src/no/hvl/dat107/Ansatt.java
+++ Dat107_Oblig3/src/no/hvl/dat107/Ansatt.java
@@ -25,6 +25,97 @@ public class Ansatt {
 	private BigDecimal manedslonn; 
 	private Avdeling tilhortAvdeling;
 	
+	public Ansatt() {
+		 
+	}
+	
+	public Ansatt (String Brukernavn, String fornavn, String etternavn, LocalDate AnsettelsesDato, String Stilling, BigDecimal manedslonn, Avdeling tilhortAvdeling) {
+		this.Brukernavn=Brukernavn;
+		this.fornavn=fornavn;
+		this.etternavn=etternavn;
+		this.AnsettelsesDato=AnsettelsesDato;
+		this.Stilling=Stilling;
+		this.manedslonn=manedslonn;
+		this.tilhortAvdeling=tilhortAvdeling;
+	}
+
+	
+	
+	
+	public int getAnsattId() {
+		return AnsattId;
+	}
+
+	public void setAnsattId(int ansattId) {
+		AnsattId = ansattId;
+	}
+
+	public String getBrukernavn() {
+		return Brukernavn;
+	}
+
+	public void setBrukernavn(String brukernavn) {
+		Brukernavn = brukernavn;
+	}
+
+	public String getFornavn() {
+		return fornavn;
+	}
+
+	public void setFornavn(String fornavn) {
+		this.fornavn = fornavn;
+	}
+
+	public String getEtternavn() {
+		return etternavn;
+	}
+
+	public void setEtternavn(String etternavn) {
+		this.etternavn = etternavn;
+	}
+
+	public LocalDate getAnsettelsesDato() {
+		return AnsettelsesDato;
+	}
+
+	public void setAnsettelsesDato(LocalDate ansettelsesDato) {
+		AnsettelsesDato = ansettelsesDato;
+	}
+
+	public String getStilling() {
+		return Stilling;
+	}
+
+	public void setStilling(String stilling) {
+		Stilling = stilling;
+	}
+
+	public BigDecimal getManedslonn() {
+		return manedslonn;
+	}
+
+	public void setManedslonn(BigDecimal manedslonn) {
+		this.manedslonn = manedslonn;
+	}
+
+	public Avdeling getTilhortAvdeling() {
+		return tilhortAvdeling;
+	}
+
+	public void setTilhortAvdeling(Avdeling tilhortAvdeling) {
+		this.tilhortAvdeling = tilhortAvdeling;
+	}
+
+	@Override
+	public String toString() {
+		return "Ansatt [AnsattId= " + AnsattId + ", Brukernavn= " + Brukernavn + ", fornavn= " + fornavn + ", etternavn="
+				+ etternavn + ", AnsettelsesDato= " + AnsettelsesDato + ", Stilling= " + Stilling + ", manedslonn= "
+				+ manedslonn + ", tilhortAvdeling= " + tilhortAvdeling + "]";
+	}
+	
+	public void skrivUt() {
+		toString();
+	}
 	
 	
 	
