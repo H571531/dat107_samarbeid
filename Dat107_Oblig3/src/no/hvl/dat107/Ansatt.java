4559254e8bd227edfef1350af889060735680a92
H571531 <31210153+H571531@users.noreply.github.com>
2018-04-04 10:55:15 +0200

    Skal skifte branch nå...

--- Dat107_Oblig3/src/no/hvl/dat107/Ansatt.java
+++ Dat107_Oblig3/src/no/hvl/dat107/Ansatt.java
@@ -1,19 +1,29 @@
 package no.hvl.dat107;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "", schema = "")
-
+@Table(name = "Ansatt", schema = "Oblig3")
 public class Ansatt {
-	private int AnsattId; //Bruker JPA for denne
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int AnsattId; //Lages av JPA
+	
+	
 	private String Brukernavn;
 	private String fornavn;
 	private String etternavn;
-	private String AnsettelsesDato; //Ma endre type!!
+	private LocalDate AnsettelsesDato; 
 	private String Stilling;
-	private double manedslonn; //OBS! OBS Endre datatype
+	private BigDecimal manedslonn; 
 	private Avdeling tilhortAvdeling;
 	private Prosjekt[] tilhorteProsjekter;
 	
