212d486a387b003fb641f5c0a4ee32d311fefcc4
H571531 <31210153+H571531@users.noreply.github.com>
2018-04-11 11:32:16 +0200

    ProsjektOSV

---
+++ Dat107_Oblig3/src/no/hvl/dat107/eao/AvdelingEAO.java
@@ -0,0 +1,5 @@
+package no.hvl.dat107.eao;
+
+public class AvdelingEAO {
+
+}
--- Dat107_Oblig3/src/no/hvl/dat107/entity/prosjektdeltagelse.java
+++ Dat107_Oblig3/src/no/hvl/dat107/entity/prosjektdeltagelse.java
@@ -43,6 +43,38 @@ public class prosjektdeltagelse {
 		prosjekt.leggTilProsjektdeltagelse(this);
 	}
 
+	public int getProsjektdeltagelse_Id() {
+		return prosjektdeltagelse_Id;
+	}
+
+	public void setProsjektdeltagelse_Id(int prosjektdeltagelse_Id) {
+		this.prosjektdeltagelse_Id = prosjektdeltagelse_Id;
+	}
+
+	public int getTimer() {
+		return timer;
+	}
+
+	public void setTimer(int timer) {
+		this.timer = timer;
+	}
+
+	public Ansatt getAnsatt() {
+		return ansatt;
+	}
+
+	public void setAnsatt(Ansatt ansatt) {
+		this.ansatt = ansatt;
+	}
+
+	public Prosjekt getProsjekt() {
+		return prosjekt;
+	}
+
+	public void setProsjekt(Prosjekt prosjekt) {
+		this.prosjekt = prosjekt;
+	}
+
 	public void skrivUt(String innrykk) {
 		System.out.printf("%sDeltagelse: %s %s, %s, %d timer", innrykk, ansatt.getFornavn(), ansatt.getEtternavn(),
 				prosjekt.getProsjektNavn(), timer);
