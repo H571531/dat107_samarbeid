6911ef2dc2ef37dbc1b2ed194dad6fed3c925f14
tohov <tohov@158.37.229.85>
2018-04-11 10:22:26 +0200

    Meny v1.2 og finnAnsMedBrukernavn

--- Dat107_Oblig3/src/no/hvl/dat107/AnsattEAO.java
+++ Dat107_Oblig3/src/no/hvl/dat107/AnsattEAO.java
@@ -5,98 +5,113 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 
 public class AnsattEAO {
-	 private EntityManagerFactory emf;
+	private EntityManagerFactory emf;
 
-	 
-	 public AnsattEAO() {
+	public AnsattEAO() {
 		emf = Persistence.createEntityManagerFactory("ansattPersistenceUnit");
-	 }
-	    public Ansatt finnAnsattMedPK(int AnsattId) {
+	}
 
-	        EntityManager em = emf.createEntityManager();
+	public Ansatt finnAnsattMedPK(int AnsattId) {
 
-	        Ansatt ansatt = null;
-	        try {
-	            ansatt = em.find(Ansatt.class, AnsattId);
-	        } finally {
-	            em.close();
-	        }
-	        return ansatt;
-	    }
+		EntityManager em = emf.createEntityManager();
 
-		public void lagAnsatt(Ansatt p) {
-			
-			EntityManager em = emf.createEntityManager();
-			EntityTransaction tx = em.getTransaction();
-
-			try {
-				tx.begin();
-				em.persist(p);
-				tx.commit();
-			
-			} catch (Throwable e) {
-				e.printStackTrace();
-				tx.rollback();
-			} finally {
-				em.close();
-			}
+		Ansatt ansatt = null;
+		try {
+			ansatt = em.find(Ansatt.class, AnsattId);
+		} finally {
+			em.close();
 		}
-		
-		
-		public List<Ansatt> finnAlleAnsatte() {
+		return ansatt;
+	}
 
-			EntityManager em = emf.createEntityManager();
+	public void lagAnsatt(Ansatt p) {
 
-			List<Ansatt> Ansatte = null;
-			try {
-				TypedQuery<Ansatt> query = em.createQuery("SELECT p FROM oblig3.ansatt p", Ansatt.class);
-				Ansatte = query.getResultList();
-			} finally {
-				em.close();
-			}
-			return Ansatte;
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+
+		try {
+			tx.begin();
+			em.persist(p);
+			tx.commit();
+
+		} catch (Throwable e) {
+			e.printStackTrace();
+			tx.rollback();
+		} finally {
+			em.close();
 		}
-		
+	}
 
-		
-		public void updateAnsatt(Ansatt p) {
+	public List<Ansatt> finnAlleAnsatte() {
 
-			EntityManager em = emf.createEntityManager();
+		EntityManager em = emf.createEntityManager();
 
-			try {
-				em.getTransaction().begin();
-				Ansatt q = em.merge(p);
-				
-				em.getTransaction().commit();
-			
-			} catch (Throwable e) {
-				e.printStackTrace();
-				em.getTransaction().rollback();
-			} finally {
-				em.close();
-			}
+		List<Ansatt> Ansatte = null;
+		try {
+			TypedQuery<Ansatt> query = em.createQuery("SELECT p FROM oblig3.ansatt p", Ansatt.class);
+			Ansatte = query.getResultList();
+		} finally {
+			em.close();
 		}
+		return Ansatte;
+	}
+	
+	public Ansatt finnAnsattMedBrukernavn(String brukernavn) {
+		EntityManager em = emf.createEntityManager();
 		
-		public void deletePerson(Ansatt p) {
-			
-			EntityManager em = emf.createEntityManager();
-
-			try {
-				em.getTransaction().begin();
-				em.remove(em.merge(p));
-				em.getTransaction().commit();
-			
-			} catch (Throwable e) {
-				e.printStackTrace();
-				em.getTransaction().rollback();
-			} finally {
-				em.close();
-			}
+		Ansatt ut = null;
+		String queryString = "SELECT a FROM Ansatt a WHERE a.brukernavn = :brukernavn";
+		
+		try {
+			TypedQuery<Ansatt> query = em.createQuery(queryString, Ansatt.class);
+			query.setParameter("brukernavn", brukernavn);
+			ut = query.getSingleResult();
+		} catch(NoResultException e) {
+			System.out.println("Finner ikke ansatt!");
+		} finally {
+			em.close();
 		}
+		return ut;
+	}
 
+	public void updateAnsatt(Ansatt p) {
+
+		EntityManager em = emf.createEntityManager();
+
+		try {
+			em.getTransaction().begin();
+			Ansatt q = em.merge(p);
+
+			em.getTransaction().commit();
+
+		} catch (Throwable e) {
+			e.printStackTrace();
+			em.getTransaction().rollback();
+		} finally {
+			em.close();
+		}
+	}
+
+	public void deletePerson(Ansatt p) {
+
+		EntityManager em = emf.createEntityManager();
+
+		try {
+			em.getTransaction().begin();
+			em.remove(em.merge(p));
+			em.getTransaction().commit();
+
+		} catch (Throwable e) {
+			e.printStackTrace();
+			em.getTransaction().rollback();
+		} finally {
+			em.close();
+		}
+	}
 
 }
--- Dat107_Oblig3/src/no/hvl/dat107/klient/Tekstgrensesnitt.java
+++ Dat107_Oblig3/src/no/hvl/dat107/klient/Tekstgrensesnitt.java
@@ -6,10 +6,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import no.hvl.dat107.Ansatt;
-import no.hvl.dat107.AnsattEAO;
-import no.hvl.dat107.Avdeling;
-import no.hvl.dat107.Prosjekt;
+import no.hvl.dat107.eao.AnsattEAO;
+import no.hvl.dat107.eao.AvdelingEAO;
+import no.hvl.dat107.eao.ProsjektEAO;
+import no.hvl.dat107.eao.ProsjektdeltakelseEAO;
+import no.hvl.dat107.entity.Ansatt;
+import no.hvl.dat107.entity.Avdeling;
+import no.hvl.dat107.entity.Prosjekt;
+import no.hvl.dat107.entity.Prosjektdeltakelse;
 
 public class Tekstgrensesnitt {
 	
@@ -26,6 +30,10 @@ public class Tekstgrensesnitt {
 		
 	}
 	
+	/**
+	 * Viser liste over alle ansatte i en tabell
+	 * @param inn Tastatur
+	 */
 	public void visAlleAnsatte(Scanner inn) {
 		
 		System.out.println("Viser alle ansatte: ");
@@ -33,7 +41,10 @@ public class Tekstgrensesnitt {
 		
 		System.out.print(lagStrek());
 		List<Ansatt> ansattListe = ansEAO.hentAlleAnsatte();
+		
+		//Hold oversikt over gyldige ansattID, for å kunne kontrollere valg av ansatt senere
 		ArrayList<Integer> gyldigId = new ArrayList<Integer>();
+		
 		for(Ansatt a: ansattListe) {
 			System.out.println(a);
 			gyldigId.add(a.getAnsattid());
@@ -41,7 +52,6 @@ public class Tekstgrensesnitt {
 		System.out.print(lagStrek());
 		
 		int valg = 0;
-		//int antAnsatte = ansattListe.size();
 		do {
 			System.out.println("Skriv ansattID for å oppdatere en ansatt, eller 0 for å gå tilbake til meny");
 			valg = inn.nextInt();
@@ -60,37 +70,77 @@ public class Tekstgrensesnitt {
 		} while(valg != 0 || valg < ansattListe.size());
 	}
 
+	/**
+	 * Søker etter en ansatt, enten med ansattID eller med brukernavn
+	 * @param inn Tastatur
+	 */
 	public void visEnAnsatt(Scanner inn) {
-		System.out.println("Viser en ansatt:");
-		
+		System.out.println("\n\nSøker etter ansatt:");
 		
 		int valg = 0;
-		
+		Ansatt ansatt = null;
+		String input = null;
 		
 		do {
-			System.out.println("Skriv ansattID til ansatt som søkes etter, eller 0 for å avbryte: ");
+			System.out.println();
+			System.out.println(lagStrek());
+			
+			System.out.println("1. Søk med ansatt-ID");
+			System.out.println("2. Søk med brukernavn");
+			System.out.println("----------------------");
+			System.out.println("0. Tilbake til meny");
+			System.out.println("\n");
+			
 			valg = inn.nextInt();
 			inn.nextLine();
-			if(valg == 0) {
+			
+			switch(valg) {
+			case 1:
+				System.out.println("Skriv ansattID som søkes etter: ");
+				ansatt = ansEAO.finnAnsattMedId(inn.nextInt());
+				break;
+			case 2:
+				System.out.println("Skriv brukernavn som søkes etter (4 tegn): ");
+				ansatt = ansEAO.finnAnsattMedBrukernavn(inn.nextLine());
+				break;
+				
+			case 0:
 				System.out.println("Går tilbake til meny...");
 				return;
+			default:
+				System.out.println("Ugyldig valg!\n\n");
 			}
 			
-			Ansatt ansatt = ansEAO.finnAnsattMedId(valg);
+			
+			
 			if(ansatt == null) {
-				System.out.println("Finner ikke ansatt med ansattID " + valg);
+				System.out.println("Finner ikke ansatt!");
 			} else {
-				System.out.print(Ansatt.lagTabellOverskrift());
+				System.out.println("Fant ansatt!");
 				System.out.print(lagStrek());
 				System.out.println(ansatt.toStringMedProsjektTimer());
 				System.out.print(lagStrek());
+				System.out.println("Oppdatere ansatt? (j/n)");
+				input = inn.next();
+				inn.nextLine();
+				if(input.equalsIgnoreCase("j")) {
+					oppdaterAnsatt(inn, ansatt.getAnsattid());
+				} else {
+					System.out.println("Går tilbake til meny...\n");
+				}
 			}
 			
-		} while(valg != 0);
+		} while(ansatt == null);
+		
 		
 		
 	}
 	
+	/**
+	 * Finner en ansatt med gitt ansattID, tilbyr valg av hvilken attributt som skal oppdateres, og oppdaterer både objekt og entitet.
+	 * @param inn Tastatur
+	 * @param ansattID ansattID til ansatt som skal oppdateres
+	 */
 	public void oppdaterAnsatt(Scanner inn, int ansattID) {
 		
 		
@@ -109,6 +159,8 @@ public class Tekstgrensesnitt {
 		System.out.println("7. Bytt avdeling");
 		System.out.println("8. Legg til prosjekt");
 		System.out.println("9. Oppdater timer på prosjekt");
+		System.out.println("10. Fjern ansatt fra prosjekt");
+		System.out.println("11. Fjern ansatt");
 		System.out.println("----------------------------");
 		System.out.println("0. Tilbake til hovedmeny");
 		
@@ -119,14 +171,17 @@ public class Tekstgrensesnitt {
 		case 1:
 			System.out.println("Angi nytt brukernavn: ");
 			a.setBrukernavn(inn.nextLine());
+			ansEAO.oppdaterAnsatt(a);
 			break;
 		case 2:
 			System.out.println("Angi nytt fornavn: ");
 			a.setFornavn(inn.nextLine());
+			ansEAO.oppdaterAnsatt(a);
 			break;
 		case 3:
 			System.out.println("Angi nytt etternavn: ");
 			a.setEtternavn(inn.nextLine());
+			ansEAO.oppdaterAnsatt(a);
 			break;
 		case 4:
 			boolean ok = false;
@@ -140,16 +195,18 @@ public class Tekstgrensesnitt {
 					System.out.println("Feil dato-format, Skriv YYYY-MM-DD, f. eks " + LocalDate.now());
 				}
 			} while(!ok);
-			
+			ansEAO.oppdaterAnsatt(a);
 			break;
 		case 5:
 			System.out.println("Angi ny stilling: ");
 			a.setStilling(inn.nextLine());
+			ansEAO.oppdaterAnsatt(a);
 			break;
 		case 6:
 			System.out.println("Angi ny månedslønn:");
 			a.setMaanedsloenn(inn.nextInt());
-			inn.nextInt();
+			inn.nextLine();
+			ansEAO.oppdaterAnsatt(a);
 			break;
 		case 7:
 			System.out.println("Tilgjengelige avdelinger: ");
@@ -174,7 +231,6 @@ public class Tekstgrensesnitt {
 			} while(avdeling == null);
 			
 			
-			
 			//Fjern fra gamle avdeling
 			Avdeling gammelAvd = a.getAnsattVed();
 			gammelAvd.getAnsattListe().remove(a);
@@ -184,17 +240,23 @@ public class Tekstgrensesnitt {
 			a.setAnsattVed(avdeling);
 			avdeling.leggTilAnsatt(a);
 			avdEAO.oppdaterAvdeling(avdeling);
+			ansEAO.oppdaterAnsatt(a);
 			break;
 		case 8:
 			System.out.println("Tilgjengelige prosjekter:");
 			
-			//Hent alle prosjekter, fjern prosjekter den ansatte allerede hører til
+			
+			//Hent alle prosjekter
+			
 			List<Prosjekt> alleProsjekter = prosjektEAO.hentAlleProsjekt();
+			//(fjern prosjekter den ansatte allerede hører til - FUNGERER IKKE)
+			/*
 			for(Prosjektdeltakelse pd: a.getProsjektDeltakelser()) {
-				if(alleProsjekter.contains(pd.getProsjektD_prosjekt())) {
-					alleProsjekter.remove(pd.getProsjektD_prosjekt());
+				if(alleProsjekter.contains(pd.getProsjekt())) {
+					alleProsjekter.remove(pd.getProsjekt());
 				}
 			}
+			*/
 			for(Prosjekt prosjekt: alleProsjekter) {
 				System.out.println(prosjekt);
 				System.out.print(lagStrek());
@@ -206,20 +268,16 @@ public class Tekstgrensesnitt {
 			
 			System.out.println("Skriv rolle den ansatte skal ha i prosjektet: ");
 			
-			Prosjektdeltakelse nyPD = new Prosjektdeltakelse(a, nyttProsjekt, inn.nextLine());
-			pdEAO.leggTilPD(nyPD);
-			a.getProsjektDeltakelser().add(nyPD);
+			//Prosjektdeltakelse nyPD = new Prosjektdeltakelse(a, nyttProsjekt, inn.nextLine(), 0);
+			pdEAO.leggTilPD(nyttProsjekt, a, inn.nextLine());
 			break;
-			
-			
-			
-			
 			
 		case 9:
 			System.out.println("Prosjekter den ansatte arbeider med: ");
 			System.out.println(a.prosjekterString());
-			System.out.println("Skriv PD-ID som skal oppdateres: ");
-			Prosjektdeltakelse pd = pdEAO.finnPDMedId(inn.nextInt());
+			System.out.println("Skriv prosjektID til prosjektdeltakelse som skal oppdateres: ");
+			Prosjekt prosjekt = prosjektEAO.finnProsjektMedId(inn.nextInt());
+			Prosjektdeltakelse pd = pdEAO.finnPDMedAnsattOgProsjekt(a, prosjekt);
 			inn.nextLine();
 			
 			System.out.println("Oppdaterer: \n" + pd);
@@ -230,29 +288,43 @@ public class Tekstgrensesnitt {
 			
 			pdEAO.oppdaterPD(pd);
 
-			//Hvis det gjøres oppdaterAnsatt(a) her forsvinner endringene for objektet - tar derfor return her
-			//Måtte eventuelt ha fjernet gamle pd og lagt inn ny pd?
-			System.out.println("Oppdaterte den ansatte:");
-			System.out.println(ansEAO.finnAnsattMedId(a.getAnsattid()).toStringMedProsjektTimer());
+			break;
+		case 10:
+			fjernProsjektdeltakelse(inn, a, null);
+			break;
+		case 11:
+			fjernAnsatt(inn, a);
 			return;
-			
 		case 0:
 			return;
+		default:
+			System.out.println("Ugyldig valg! ");
+			break;
 		}
 		
-		ansEAO.oppdaterAnsatt(a);
+		//ansEAO.oppdaterAnsatt(a);
 		
 		System.out.println("Oppdaterte den ansatte:");
 		System.out.println(ansEAO.finnAnsattMedId(a.getAnsattid()).toStringMedProsjektTimer());
 	}
 	
+	/**
+	 * Legger til en ansatt, tar inn opplysninger gjennom tastaturet
+	 * @param inn Tastatur
+	 */
 	public void leggTilAnsatt(Scanner inn) {
 		
-		
 		System.out.println("Legger til ny ansatt");
-		System.out.println("Skriv brukernavn (4 tegn): ");
 		
-		String brukernavn = inn.nextLine();
+		String brukernavn = null;
+		System.out.println("Skriv brukernavn (4 tegn): ");
+		do {
+			brukernavn = inn.nextLine();
+			if(brukernavn.length() != 4) {
+				System.out.println("Brukernavn må ha 4 tegn!");
+			}
+		} while(brukernavn.length() != 4);
+		
 		
 		System.out.println("Skriv fornavn: ");
 		String fornavn = inn.nextLine();
@@ -293,13 +365,31 @@ public class Tekstgrensesnitt {
 		Ansatt nyAnsatt = new Ansatt(brukernavn, fornavn, etternavn, ansDato, stilling, loenn, avdeling);
 		
 		
-		ansEAO.settInnAnsatt(nyAnsatt);
+		int nyId = ansEAO.settInnAnsatt(nyAnsatt);
 		
 		//avdeling.getAnsatte().add(nyAnsatt);
-		avdEAO.oppdaterAvdeling(avdeling);
+		//avdEAO.oppdaterAvdeling(avdeling);
+		
+		System.out.println(ansEAO.finnAnsattMedId(nyId).toStringMedProsjektTimer());
 		
 	}
 	
+	public void fjernAnsatt(Scanner inn, Ansatt ans) {
+		//Ikke lov å slette ansatt hvis ansatt er sjef, eller hvis ansatt har registrert timer i et prosjekt
+		if(ans.getSjefFor() != null) {
+			System.out.println("Kan ikke slette ansatt som er sjef!");
+		} else if(ans.totaleTimer() > 0) {
+			System.out.println("Kan ikke slette ansatt som har registrerte timer i et prosjekt!");
+		} else {
+			System.out.println("Sletter ansatt...");
+			ansEAO.fjernAnsatt(ans);
+		}
+	}
+	
+	/**
+	 * Henter alle avdelinger i databasen og viser disse, inkludert ansatte, i tabeller
+	 * @param inn Tastatur
+	 */
 	public void visAlleAvdelinger(Scanner inn) {
 		System.out.println("Viser alle avdelinger: ");
 		List<Avdeling> avdelinger = avdEAO.hentAlleAvdelinger();
@@ -331,22 +421,58 @@ public class Tekstgrensesnitt {
 		} while(!gyldigeAvdelinger.contains(valg));
 	}
 	
+	/**
+	 * Søker etter en avdeling med avdelingsID, og viser denne i konsoll
+	 * @param inn Tastatur
+	 */
 	public void visEnAvdeling(Scanner inn) {
-		System.out.println("Viser en avdeling");
-		System.out.println("Skriv avdelingsID: ");
-		int avdeling = inn.nextInt();
-		inn.nextLine();
 		
-		System.out.println(avdEAO.finnAvdelingMedId(avdeling));
+		int avdelingsID = -1;
+		String input = null;
+		
+		Avdeling avdeling = null;
+		System.out.println("Viser en avdeling");
+		do{
+			System.out.println("Skriv avdelingsID, eller 0 for å avbryte søk: ");
+			
+			avdelingsID = inn.nextInt();
+			inn.nextLine();
+			if(avdelingsID == 0) {
+				return;
+			}
+			
+			avdeling = avdEAO.finnAvdelingMedId(avdelingsID);
+			
+			if(avdeling == null) {
+				System.out.println("Finner ikke avdeling!");
+			} else {
+				System.out.println("Fant avdeling!");
+				System.out.print(lagStrek());
+				System.out.println(avdeling);
+				System.out.print(lagStrek());
+				System.out.println("Oppdatere avdeling? (j/n)");
+				input = inn.next();
+				inn.nextLine();
+				if(input.equalsIgnoreCase("j")) {
+					oppdaterAnsatt(inn, avdeling.getAvdelingsID());
+				} else {
+					System.out.println("Går tilbake til meny...\n");
+					avdelingsID = 0;
+				}
+			}
+		} while(avdelingsID != 0);
+		
 	}
 	
+	/**
+	 * Finner en avdeling med gitt avdelingsID, og oppdaterer enten avdelingens navn, eller avdelingens sjef, gjennom tastaturet.
+	 * @param inn Tastatur
+	 * @param avdID AvdelingsID til avdeling som skal oppdateres
+	 */
 	public void oppdaterAvdeling(Scanner inn, int avdID) {
 		System.out.println("Oppdaterer avdeling");
 		
-		
-		
 		Avdeling avdeling = avdEAO.finnAvdelingMedId(avdID);
-		
 		
 		System.out.println("Avdeling som skal oppdateres: " + avdeling);
 		System.out.println("-------------------------------");
@@ -361,6 +487,7 @@ public class Tekstgrensesnitt {
 		case 1:
 			System.out.println("Angi nytt avdelingsnavn");
 			avdeling.setNavn(inn.nextLine());
+			avdEAO.oppdaterAvdeling(avdeling);
 			break;
 		case 2:
 			System.out.println("Skriv ansattID på ansatt som skal være ny sjef");
@@ -381,36 +508,25 @@ public class Tekstgrensesnitt {
 				nySjef.setAnsattVed(avdeling);
 				avdeling.getAnsattListe().add(nySjef);
 			}
-				
-			Ansatt gammelSjef = avdeling.getSjef();
 			
-			avdeling.setSjef(nySjef);
-			avdEAO.oppdaterAvdeling(avdeling);
-			
-			//nySjef.setSjefFor(avdeling);
-			nySjef.setStilling("Sjef A" + avdeling.getAvdelingsID());
-			ansEAO.oppdaterAnsatt(nySjef);
-			
-			//Oppdater gammelSjef
-			
-			//gammelSjef.setSjefFor(null);
-			gammelSjef.setStilling("Tidl. sjef");
-			ansEAO.oppdaterAnsatt(gammelSjef);
-			
+			avdEAO.oppdaterNySjef(avdeling, nySjef);
 			
 			break;
 			
 		}
-		//avdEAO.oppdaterAvdeling(avdeling);
+		//
 		System.out.println("Avdeling etter oppdatering: ");
 		System.out.println(avdeling);
 		//Får ikke vist oppdatert avdeling med bare å bare printe avdeling her
 		System.out.println(avdEAO.finnAvdelingMedId(avdeling.getAvdelingsID()));
+		
+		
 	}
 	
-			
-	
-	
+	/**
+	 * Viser alle prosjekter, inkludert ansatte og timetall		
+	 * @param inn Tastatur
+	 */
 	public void visAlleProsjekter(Scanner inn) {
 		System.out.println("Viser alle prosjekter: ");
 		List<Prosjekt> prosjekter = prosjektEAO.hentAlleProsjekt();
@@ -442,6 +558,10 @@ public class Tekstgrensesnitt {
 		} while(valg != 0);
 	}
 	
+	/**
+	 * Legger til nytt prosjekt i databasen. Tar inn opplysninger gjennom tastatur
+	 * @param inn Tastatur
+	 */
 	public void leggTilProsjekt(Scanner inn) {
 		
 		System.out.println("Legger til prosjekt");
@@ -457,12 +577,17 @@ public class Tekstgrensesnitt {
 		
 	}
 	
+	/**
+	 * Oppdaterer prosjekt-attributt, som bestemmes gjennom tastatur
+	 * @param inn Tastatur
+	 * @param prosjektid ProsjektID til prosjekt som skal oppdateres
+	 */
 	public void oppdaterProsjekt(Scanner inn, int prosjektid) {
 		System.out.println("Oppdaterer prosjekt");
 		
 		Prosjekt prosjekt = prosjektEAO.finnProsjektMedId(prosjektid);
-		System.out.println("Prosjekt som skal oppdateres: ");
-		System.out.println(prosjekt);
+		System.out.println("Prosjekt som skal oppdateres: \n");
+		System.out.println(prosjekt + "\n");
 		
 		int valg = 0;
 		
@@ -471,6 +596,7 @@ public class Tekstgrensesnitt {
 			System.out.println("2. Oppdater prosjektbeskrivelse");
 			System.out.println("3. Legg til en ansatt til et prosjekt");
 			System.out.println("4. Oppdater timer på prosjekt for en ansatt");
+			System.out.println("5. Fjern en ansatt");
 			System.out.println(lagStrek());
 			System.out.println("0. Avbryt");
 			
@@ -480,38 +606,36 @@ public class Tekstgrensesnitt {
 			
 			switch(valg) {
 			case 1:
-				System.out.println("Skriv nytt prosjeknavn: ");
+				//Oppdatere navn
+				System.out.println("Skriv nytt prosjektnavn: ");
 				prosjekt.setProsjektnavn(inn.nextLine());
+				prosjektEAO.oppdaterProsjekt(prosjekt);
 				break;
 			case 2:
+				//Oppdatere beskrivelse
 				System.out.println("Skriv ny prosjektbeskrivelse: ");
 				prosjekt.setBeskrivelse(inn.nextLine());
+				prosjektEAO.oppdaterProsjekt(prosjekt);
 				break;
 			case 3:
+				//Legg til ansatt
 				System.out.println("Skriv ansattid på ansatt som skal legges til: ");
 				Ansatt ans = ansEAO.finnAnsattMedId(inn.nextInt());
 				inn.nextLine();
 				System.out.println("Skriv rolle den ansatte skal ha i prosjektet: ");
-				Prosjektdeltakelse nyDeltakelse = new Prosjektdeltakelse(ans, prosjekt, inn.nextLine());
-				pdEAO.leggTilPD(nyDeltakelse);
+				
+				pdEAO.leggTilPD(prosjekt, ans, inn.nextLine());
 				break;
 			case 4:
+				//Oppdatere timetall
 				System.out.println("Ansatte som arbeider på " + prosjekt.getProsjektnavn());
-				System.out.println("\t" + Ansatt.lagTabellOverskrift());
-				System.out.println(Tekstgrensesnitt.lagStrek());
-				
-				for(Prosjektdeltakelse pd: prosjekt.getAnsatte()) {
-					
-					System.out.println(pd.getDeltakelseid() + " \t" + pd.getProsjektD_ansatt() + " - " + pd.getAntallTimer() + " timer");
-					
-				}
-				System.out.println(Tekstgrensesnitt.lagStrek());
-				System.out.println("Skriv PDID som skal oppdateres: ");
-				//Ansatt ansatt = ansEAO.finnAnsattMedId(inn.nextInt());
-				Prosjektdeltakelse pdTilOppdatering = pdEAO.finnPDMedId(inn.nextInt());
+				prosjekt.visAnsatte();
+				System.out.println("Skriv ansattID til ansatt som skal oppdateres: ");
+				Ansatt ansatt = ansEAO.finnAnsattMedId(inn.nextInt());
+				Prosjektdeltakelse pdTilOppdatering = pdEAO.finnPDMedAnsattOgProsjekt(ansatt, prosjekt);
 				inn.nextLine();
 				
-				System.out.println("PD som skal oppdateres: " + pdTilOppdatering);
+				System.out.println("Prosjekt som skal oppdateres: " + pdTilOppdatering);
 				
 				System.out.println("Skriv hvor mange timer den ansatte har arbeidet med prosjektet: ");
 				int antallTimer = inn.nextInt();
@@ -520,27 +644,53 @@ public class Tekstgrensesnitt {
 				pdTilOppdatering.setAntallTimer(antallTimer);
 				pdEAO.oppdaterPD(pdTilOppdatering);
 				
-				System.out.println("Etter oppdatering: ");
-				for(Prosjektdeltakelse pd: prosjekt.getAnsatte()) {
-					
-					System.out.println(pd.getProsjektD_ansatt() + " - " + pd.getAntallTimer() + " timer");
-					
-				}
+				
+				break;
+			case 5:
+				fjernProsjektdeltakelse(inn, null, prosjekt);
+				break;
 			case 0:
 				return;
 				
 			}
+			System.out.println("Prosjekt etter oppdatering:");
+			System.out.println(lagStrek());
+			System.out.println(prosjektEAO.finnProsjektMedId(prosjekt.getProsjektid()));
 		} while(valg != 0);
+	}
+	
+	public void fjernProsjektdeltakelse(Scanner inn, Ansatt ans, Prosjekt prosjekt) {
+		if(prosjekt == null) {
+			System.out.println("Prosjekter den ansatte er registrert ved: ");
+			System.out.println(ans.prosjekterString());
+			System.out.println("\nAngi prosjekt den ansatte skal fjernes fra:");
+			prosjekt = prosjektEAO.finnProsjektMedId(inn.nextInt());
+			inn.nextLine();
+		} else if(ans == null) {
+			System.out.println("Ansatte som arbeider på prosjektet:");
+			prosjekt.visAnsatte();
+			System.out.println("\nAngi ansatt som skal fjernes fra prosjektet:");
+			ans = ansEAO.finnAnsattMedId(inn.nextInt());
+			inn.nextLine();
+		}
 		
+		System.out.println("Fjerner prosjektdeltakelse...\n");
+		pdEAO.fjernProsjektdeltakelse(ans, prosjekt);
 		
-		
-		
-		
-		
+		//Viser oppdatert prosjekt/ansatt i kallende metode, ikke nødvendig å vise her
+		//System.out.println("Prosjekt etter fjerning:\n" + prosjektEAO.finnProsjektMedId(prosjekt.getProsjektid()));
+		//System.out.println("\nDen ansatte etter fjerning:\n" + ansEAO.finnAnsattMedId(ans.getAnsattid()));
+	
 		
 	}
 	
+	/**
+	 * Gir en String med 150 "-"
+	 * @return Skillestrek
+	 */
 	public static String lagStrek() {
+		//Kunne vært public void som printet en strek - men bruker i opprettelse og formatering av tabeller,
+		//derfor nyttig med String
 		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < 150; i++) {
 			sb.append("-");
